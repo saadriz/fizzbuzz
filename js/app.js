c5c7bed948d4014ebe65c35b173d49b676ad32d5
saadriz <urizvsa@44935-plc-ex-lt.home>
2014-07-21 16:23:58 -0400

    Refectored

--- js/app.js
+++ js/app.js
@@ -1,26 +1,35 @@
 $(document).ready(function() {
   
    $(".submit").click(function(){
-      var number = $('input[name=input-form]').val();
-      
-      for (var i=1; i<=(number); i++) {
+
+    var number = +$('input[name=input-form]').val();
+    
+    $(".list").find('li').remove();
+    
+    if (number >= 1 && number <=100) {
+
+    for (var i=1; i<=(number); i++) {
         if (i%15==0) {
 
-          $(".list").append('<p>'+ "fizzbuzz" + '</p>');
+          $(".list").append('<li>'+ "fizzbuzz" + '</li>');
         }
         else if (i%3==0) {
 
-          $(".list").append('<p>'+ "fizz" + '</p>');
+          $(".list").append('<li>'+ "fizz" + '</li>');
         } else if (i%5==0) {
-        $(".list").append('<p>'+ "buzz" + '</p>');
+        $(".list").append('<li>'+ "buzz" + '</li>');
     
         }
 
         else {
-        $(".list").append('<p>'+ i + '</p>');
+        $(".list").append('<li>'+ i + '</li>');
     
         }
        }
+     } else {
+      alert("Please enter a number between 1 and 100")
+    };
+     
    });
 
 
